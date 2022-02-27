87085f7717dd76819154cabbf87112f584c91716
Joshi <Joshi.Gaurav@bcgplatinion.com>
2022-02-27 06:19:26 +0100

    API update

--- test/__init__.py
+++ test/__init__.py
@@ -16,36 +16,35 @@ DATA_PARSER = REST_APIS.parser()
 DATA_PARSER.add_argument('name', location='form', type=str, required=True)
 DATA_PARSER.add_argument('description', location='form', type=str, required=True)
 
-TestREST_APISs = Namespace('/Test', description='Test Microservices: CRUD operations')
+CRUD_APISs = Namespace('/crud', description='Test Microservices: CRUD operations')
+REST_APIS.add_namespace(CRUD_APISs, path='/crud')
 
-REST_APIS.add_namespace(TestREST_APISs, path='/Test')
-
-@TestREST_APISs.route("/")
+@CRUD_APISs.route("/")
 class TestREST_APIS(Resource):
     def get(self):
         """Delete Data"""
         return "Got all Data successfully", 200
 
 
-    @TestREST_APISs.expect(DATA_PARSER)
-    @TestREST_APISs.response(200, 'Data added successfully')
-    @TestREST_APISs.response(400, 'Bad request')
+    @CRUD_APISs.expect(DATA_PARSER)
+    @CRUD_APISs.response(200, 'Data added successfully')
+    @CRUD_APISs.response(400, 'Bad request')
     def post(self):
         """Create Data"""
         return "Data added successfully", 200
 
 
-    @TestREST_APISs.expect(DATA_PARSER)
-    @TestREST_APISs.response(200, 'Data updated successfully')
-    @TestREST_APISs.response(400, 'Bad request')
+    @CRUD_APISs.expect(DATA_PARSER)
+    @CRUD_APISs.response(200, 'Data updated successfully')
+    @CRUD_APISs.response(400, 'Bad request')
     def put(self):
         """Update Data"""
         return "Data updated successfully", 200
 
 
-    @TestREST_APISs.expect(DATA_PARSER)
-    @TestREST_APISs.response(200, 'Data deleted successfully')
-    @TestREST_APISs.response(400, 'Bad request')
+    @CRUD_APISs.expect(DATA_PARSER)
+    @CRUD_APISs.response(200, 'Data deleted successfully')
+    @CRUD_APISs.response(400, 'Bad request')
     def delete(self):
         """Deleted data"""
         return "Data deleted successfully", 200
