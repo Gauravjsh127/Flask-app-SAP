3ea7687b63947c114308ad13f9924297761a9d16
Joshi <Joshi.Gaurav@bcgplatinion.com>
2022-02-27 06:17:25 +0100

    API update

--- test/__init__.py
+++ test/__init__.py
@@ -16,22 +16,36 @@ DATA_PARSER = REST_APIS.parser()
 DATA_PARSER.add_argument('name', location='form', type=str, required=True)
 DATA_PARSER.add_argument('description', location='form', type=str, required=True)
 
-TestREST_APISs = Namespace('/Test', description='Endpoints available to manage Test Micro services')
+TestREST_APISs = Namespace('/Test', description='Test Microservices: CRUD operations')
 
 REST_APIS.add_namespace(TestREST_APISs, path='/Test')
 
 @TestREST_APISs.route("/")
 class TestREST_APIS(Resource):
     def get(self):
-        """Endpoint to get all the Data"""
-        return "Test", 200
+        """Delete Data"""
+        return "Got all Data successfully", 200
 
 
     @TestREST_APISs.expect(DATA_PARSER)
     @TestREST_APISs.response(200, 'Data added successfully')
     @TestREST_APISs.response(400, 'Bad request')
     def post(self):
-        """Add a new Post request"""
-        return "Test", 200
+        """Create Data"""
+        return "Data added successfully", 200
 
 
+    @TestREST_APISs.expect(DATA_PARSER)
+    @TestREST_APISs.response(200, 'Data updated successfully')
+    @TestREST_APISs.response(400, 'Bad request')
+    def put(self):
+        """Update Data"""
+        return "Data updated successfully", 200
+
+
+    @TestREST_APISs.expect(DATA_PARSER)
+    @TestREST_APISs.response(200, 'Data deleted successfully')
+    @TestREST_APISs.response(400, 'Bad request')
+    def delete(self):
+        """Deleted data"""
+        return "Data deleted successfully", 200
